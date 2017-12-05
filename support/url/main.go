1bb6f9b652befb7b98d1ceaaeabe1d01d98e688e
Tomer Weller <tomer.weller@gmail.com>
2017-12-05 14:46:34 -0800

    improve documentation

--- support/url/main.go
+++ support/url/main.go
@@ -4,9 +4,10 @@ import (
 	gUrl "net/url"
 )
 
+// URL wraps around the native golang URL struct to allow for custom methods
 type URL gUrl.URL
 
-// SetParam
+// SetParam returns a new URL with the given param created or modified if already exists
 func (u URL) SetParam(key string, val string) URL {
 	gu := gUrl.URL(u)
 	q := gu.Query()
@@ -16,11 +17,13 @@ func (u URL) SetParam(key string, val string) URL {
 	return URL(gu)
 }
 
+// String encodes all URL segments to a fully qualified URL string
 func (u URL) String() string {
 	gu := gUrl.URL(u)
 	return gu.String()
 }
 
+// Parse decodes a URL string. Returns error if string is not a legal
 func Parse(s string) (u URL, err error) {
 	gu, err := gUrl.Parse(s)
 	if err != nil {
